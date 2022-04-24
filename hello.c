71d7cac12ceab33a8e724be3340d9372a9589dd6
mutian <yinmutian@bupt.edu.cn>
2022-04-24 23:27:43 +0800

    helloworld!
    test,just a test!

--- hello.c
+++ hello.c
@@ -3,6 +3,30 @@
 
 main(){
     printf("hello world\n");
+    int arr[] = {0,3,2,4,7,2,4,63,6,20,5,43,10};
+    int i,j;
+    int length = sizeof(arr)/sizeof(arr[0]);
+    for(i=0;i<length-1;i++){
+        for(j=0;j<length-1-i;j++){
+            int temp;
+            if(arr[j] > arr[j+1]){
+                temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(i=0;i<length;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    //冒泡排序
+
+
+
+
+
+
     system("pause");
   
     return 0;
